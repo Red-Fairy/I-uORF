ed978c45f97006a619624765fc119d1ba757b918
Rundong Luo <redfairy@viscam1.stanford.edu>
2023-03-30 01:31:24 -0700

    move deduction before locality constraint

--- models/model_T.py
+++ models/model_T.py
@@ -333,7 +333,7 @@ class SlotAttention(nn.Module):
         self.slots_logsigma_bg = nn.Parameter(torch.zeros(1, 1, slot_dim))
         init.xavier_uniform_(self.slots_logsigma_bg)
 
-        if self.learnable_pos:
+        if learnable_pos:
             self.fg_position = nn.Parameter(torch.rand(1, num_slots-1, 2) * 2 - 1)
         else:
             self.fg_position = None
